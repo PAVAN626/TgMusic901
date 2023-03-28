78031352738fa3b97e105bd21c2eaa1175e59f90
VAMSI REDDY <118047800+PAVAN626@users.noreply.github.com>
2023-03-29 03:34:29 +0530

    Update ping.py

--- ShizukaXMusic/plugins/tools/ping.py
+++ ShizukaXMusic/plugins/tools/ping.py
@@ -28,5 +28,8 @@ async def ping_com(client, message: Message, _):
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
-        _["ping_2"].format(resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping)
+        _["ping_2"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ),
+        reply_markup=close_keyboard
     )
